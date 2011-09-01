674b9c7e91e51cf584f3941c176035b6890091ac
Chris Olstrom <chris@olstrom.com>
2011-09-01 15:01:25 -0700

    Modified shebangs to be more path-agnostic.

--- batch_convert.pl
+++ batch_convert.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 #########################################################
 # Batch Convert, an addon for Loran T. Hughes' 'mp3ogg' #
--- batch_convert.sh
+++ batch_convert.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 # Thanks to koala_man, from #linux on irc.freenode.net, for this.
 find . -type d | while read dir; do (cd "$dir"; mp3ogg;); done
